0496e0436a45f045f8f850712c512c48625fce3f
Daniel Anderson <daniel.r.anderson2013@gmail.com>
2014-11-10 12:47:44 -0600

    Added another NFA with tests.
    New NFA defined, with all tests passing. Now the JSON parser NFA
    doesn't require an array of a single result state of a move

--- project/finite_automata/jsonToNFA.py
+++ project/finite_automata/jsonToNFA.py
@@ -4,7 +4,11 @@ def createNFA(theJSON):
 	theJSON = json.loads(theJSON)
 	# todo
 	# valid = verifyNFA(theJSON)
-	theNFA = NFA(set(theJSON['states']), set(theJSON['alphabet']), theJSON['startState'], theJSON['finalStates'])
+	states = set(theJSON['states'])
+	alphabet = set(theJSON['alphabet'])
+	startState = theJSON['startState']
+	finalStates = theJSON['finalStates']
+	theNFA = NFA(states, alphabet, startState, finalStates)
 	addMovesFromJSON(theJSON, theNFA)
 	return theNFA
 
@@ -12,10 +16,19 @@ def addMovesFromJSON(theJSON, theNFA):
 	moves = theJSON['moves']
 	for state in moves:
 		for inputSymbol in moves[state]:
-			for result in moves[state][inputSymbol]:
+			#Converts string to an array including only that string
+			#leaves arrays alone
+			results = convertToList(moves[state][inputSymbol])
+			for result in results:
 				theNFA.addMove(state, inputSymbol, result)
 
 def verifyNFA(theJSON):
 	# todo
 	return true
+
+def convertToList(stringOrList):
+	if type(stringOrList) is list:
+		return stringOrList
+	else:
+		return [stringOrList]
 
--- project/testing/testNFA.py
+++ project/testing/testNFA.py
@@ -23,6 +23,38 @@ class TestNFA(unittest.TestCase):
 							}
 					}'''
 		self.machine1 = jsonToNFA.createNFA(self.json1)
+		
+		self.json2 = '''
+		{
+			"states": ["q0", "q1", "q2", "q3", "q4", "q5"],
+			"alphabet": ["a"],
+			"startState": "q0",
+			"finalStates": ["q4"],
+			"moves":
+				{
+					"q0": {
+							"a": "q1"
+						},
+					"q1": {
+							"a": "q2"
+						},
+					"q2": {
+							"a": "q3"
+						},
+					"q3": {
+							"a": "q4"
+						},
+					"q4": {
+							"a": "q5"
+						},
+					"q5": {
+							"a": "q0"
+						}
+				}
+
+		}
+		'''
+		self.machine2 = jsonToNFA.createNFA(self.json2)
 
 	def test_accept_NFA1(self):
 		self.assertTrue(self.machine1.isInLanguage('aaabbaaabb'))
@@ -35,3 +67,14 @@ class TestNFA(unittest.TestCase):
 	def test_JSON_parser(self):
 		# Note, we are just calling the function here. That is to test to see that the function doesn't fail
 		jsonToNFA.createNFA(self.json1)
+	
+	def test_accept_NFA2(self):
+		self.assertTrue(self.machine2.isInLanguage('aaaaaaaaaa'))
+	def test_deny_NFA2(self):
+		self.assertFalse(self.machine2.isInLanguage('aaaaa'))
+	def test_empty_NFA2(self):
+		self.assertFalse(self.machine2.isInLanguage(''))
+	def test_raises_NFA2(self):
+		self.assertRaises(AssertionError, self.machine2.isInLanguage, 'aaaaaaaaaaaab')
+	def test_JSON_parser2(self):
+		jsonToNFA.createNFA(self.json2)
