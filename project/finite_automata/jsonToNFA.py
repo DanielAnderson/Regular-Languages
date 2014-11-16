b02540e135a0a636a2919c9cc9cb4068499cfeeb
Daniel Anderson <daniel.r.anderson2013@gmail.com>
2014-11-16 00:54:54 -0600

    Added verification for loading of NFA's from JSON
    Loaded NFA's now verified for legality. As of now, the verification
    allows all test cases. Next commit will be checking that it doesn't
    allow illegal NFA's

--- project/finite_automata/jsonToNFA.py
+++ project/finite_automata/jsonToNFA.py
@@ -2,8 +2,7 @@ import json
 from project.finite_automata.NFA import NFA
 def createNFA(theJSON):
 	theJSON = json.loads(theJSON)
-	# todo
-	# valid = verifyNFA(theJSON)
+	verifyNFA(theJSON)
 	states = set(theJSON['states'])
 	alphabet = set(theJSON['alphabet'])
 	startState = theJSON['startState']
@@ -14,6 +13,7 @@ def createNFA(theJSON):
 
 def addMovesFromJSON(theJSON, theNFA):
 	moves = theJSON['moves']
+
 	for state in moves:
 		for inputSymbol in moves[state]:
 			#Converts string to an array including only that string
@@ -23,8 +23,15 @@ def addMovesFromJSON(theJSON, theNFA):
 				theNFA.addMove(state, inputSymbol, result)
 
 def verifyNFA(theJSON):
-	# todo
-	return true
+	assert theJSON['startState'] in theJSON['states']
+	assert 'lambda' not in theJSON['alphabet']
+	moves = theJSON['moves']
+	for state in moves:
+		assert state in theJSON['states']
+		for alphabetMember in moves[state]:
+			assert alphabetMember in theJSON['alphabet'] or alphabetMember is 'lambda' 
+			for nextState in convertToList(moves[state][alphabetMember]):
+				assert nextState in theJSON['states']
 
 def convertToList(stringOrList):
 	if type(stringOrList) is list:
