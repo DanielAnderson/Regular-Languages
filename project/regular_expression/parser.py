0d1ecd2e1ec4b11b4f8c7a81cf0a598af26a2152
Daniel Anderson <daniel.r.anderson2013@gmail.com>
2014-11-23 21:02:40 -0600

    Deleted debug statements from parser

--- project/regular_expression/parser.py
+++ project/regular_expression/parser.py
@@ -1,12 +1,9 @@
 '''Parses a string including parentheses'''
 def parenthesesParser(string):
-    print('ARGS: ' + string)
     parsed = __parenParser__(string)
-    print('PARSED' + str(parsed))
     ans = []
     if len(parsed['left']) > 0:
         ans.append(parsed['left'])
-    # print(parsed)
     if '(' in parsed['middle']:
         ans.append(parenthesesParser(parsed['middle']))
     else:
@@ -17,7 +14,6 @@ def parenthesesParser(string):
     elif len(parsed['remainder']) > 0:
         ans.append(parsed['remainder'])
 
-    print('ANSWER: ' + ans.__str__())
     if len(ans) == 1 and len(ans[0]) == 1:
         return ans[0]
     else:
@@ -25,11 +21,9 @@ def parenthesesParser(string):
 
 '''Given a string <x>(<y>)<z> returns {'left': <x>, 'middle': <y>, 'remainder': <z>}'''
 def __parenParser__(string):
-    # print('__parenParser__ ARGS: ' + string)
     ans = dict()
     ans['left'], leftLength = __leftOfParen__(string)
     ans['middle'], ans['remainder'] = __matchLeadingParen__(string[leftLength:])
-    # print('__parenParser__ ANS: ' + str(ans))
     return ans
 
 '''Given string <x>(<z>, returns (<x>, len(x))'''
