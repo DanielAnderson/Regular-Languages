aa96947066764268651536a361577d13dc03da24
Christina7 <147.christina.s@gmail.com>
2014-11-28 18:46:27 -0600

    Kivy download instructions
    Kivy download instructions. Works on Windows, not sure about other OS

--- project/gui/gui.py
+++ project/gui/gui.py
@@ -1,1 +1,7 @@
-Dummy file to force git to recognize directory
+#We are going to be using Kivy for a GUI
+#First download pygame then download Kivy. Here is the download site:
+#http://www.lfd.uci.edu/~gohlke/pythonlibs/
+#I tried downloading from the Kivy site but had lots of issues, 
+#this site allowed the downloads to work on Windows OS and worked with Kivy's test code.
+
+
