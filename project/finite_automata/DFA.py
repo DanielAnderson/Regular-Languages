029b58bfde552039e2d33b45a175c2af72bde55a
Daniel Anderson <daniel.r.anderson2013@gmail.com>
2014-12-08 17:12:27 -0600

    Added state generation to DFA and (passing) tests

--- project/finite_automata/DFA.py
+++ project/finite_automata/DFA.py
@@ -1,3 +1,20 @@
 class DFA:
     def __init__(self, equivNFA):
-        pass
+        self.myNFA = equivNFA
+        startState = set()
+        startState.add(equivNFA.startState)
+        
+        self.startState = frozenset(startState)
+        self.states = set()
+        
+        toAdd = list()
+        toAdd.append(self.startState)
+
+        while(len(toAdd) > 0):
+            state = toAdd.pop()
+            if(state not in self.states):
+                self.states.add(state)
+                for character in self.myNFA.alphabet:
+                    toAdd.append(frozenset(self.myNFA.applyTransition(state, character)))
+
+
